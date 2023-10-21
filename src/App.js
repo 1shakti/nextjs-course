690820841ea5649d67acc3377f0194b2a58c2267
1shakti <shakti26.sj@gmail.com>
2023-10-21 18:29:25 +0530

    created nav bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FavouritesPage from "./pages/Favourites";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./component/layouts/MainNavigation";
 
 const  App = () => {
   return (
     <Router>
+      <MainNavigation />
       <Routes>
-        <Route path="/" element={<AllMeetupsPage />} />
+        <Route exact path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
         <Route path="/favourites" element={<FavouritesPage />} />
       </Routes>
