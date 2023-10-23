d91e5a0e4b139f2adadcab10998affc4f64745e1
1shakti <shakti26.sj@gmail.com>
2023-10-23 16:58:08 +0530

    created allmeetup cards with dummy data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,7 +1,11 @@
+import MeetupList from "../component/meetups/MeetupList";
+import allMeetupData from "../constant/allmeetups.json";
+
 const AllMeetupsPage = () => {
     return (
         <div>
-            <h1>All Meet up Page</h1>
+            <h1>All Meetups</h1>
+            <MeetupList meetups={allMeetupData} />
         </div>
     )
 }
