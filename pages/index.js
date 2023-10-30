46372dec1df81e436dff52b70195c1fef28fcaad
1shakti <shakti26.sj@gmail.com>
2023-10-30 22:23:39 +0530

    created event list and item component, dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import styles from '@/styles/Home.module.css'
+import EventList from '@/components/events/event-list';
+import { getFeaturedEvents } from '@/dummy-data';
 
 export default function Home() {
+  
+  const featuredEvents = getFeaturedEvents();
 
   return (
-    <div className={styles.container}>
-        <h1>Hello Next World!</h1>
+    <div>
+      <EventList items={featuredEvents} />
     </div>
   )
 }
