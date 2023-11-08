ed6126981b8a001165ff9401852b4334546d1c29
1shakti <shakti26.sj@gmail.com>
2023-11-08 12:19:14 +0530

    created nav baar

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,13 @@
+import EventList from "@/components/events/event-list";
+import { getAllEvents } from "@/dummy-data";
+
 const AllEventsPage = () => {
+    const events =  getAllEvents();
+
     return (
-        <p>Events page</p>
+        <div>
+            <EventList items={events} />
+        </div>
     )
 } 
 
