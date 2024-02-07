a92f3e3cdc6ea746d3e9f033ddc99d708409dd6d
1shakti <shakti26.sj@gmail.com>
2024-02-07 18:06:43 +0530

    created feedback submit api route and feedback form

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,42 @@
-import EventList from '@/components/events/event-list';
-import { getFeaturedEvents } from '@/helpers/api-utils';
+import { useRef } from "react"
 
-export default function Home(props) {
-  const { events:featuredEvents } = props;
+export default function Home() {
+  const emailInputRef = useRef(null); 
+  const feedbackInputRef = useRef(null);
+
+  const submitFeedbackHandler = (event) => {
+    event.preventDefault();
+
+        const enteredEmail = emailInputRef.current.value;
+        const enteredFeedback = feedbackInputRef.current.value;
+    
+        const reqBody = { email: enteredEmail, text: enteredFeedback }
+  
+        fetch('/api/feedback',{
+          method:"POST",
+          body: JSON.stringify(reqBody),
+          headers:{
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+  }
+
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <h1>The Home Page</h1>
+      <form onSubmit={submitFeedbackHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" name="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Email Address</label>
+          <textarea id='feedback' rows='5' ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Submit Feedback</button>
+      </form>
     </div>
   )
-}
-
-export async function getStaticProps(){
-  const featuredEvents = await getFeaturedEvents();
-  return {
-    props: {
-      events: featuredEvents,
-      revalidate: 1800
-    }
-  }
 }
