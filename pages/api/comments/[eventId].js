c551b1bc6013a7a4de890ac588bb29acc7fef3dd
1shakti <shakti26.sj@gmail.com>
2024-02-15 15:06:58 +0530

    used mongodb to store and show newsletter and comments

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,36 +1,82 @@
 import fs from "fs";
-import { buildfilePath, extractFileData } from "@/helpers/file-path-extractData";
+import {
+	buildfilePath,
+	extractFileData,
+} from "@/helpers/file-path-extractData";
+import {
+	connectDatabase,
+	getAllDocuments,
+	insertDocument,
+} from "@/helpers/db-utils";
 
-export default function handler(req, res){
+export default async function handler(req, res) {
+	const eventId = req.query.eventId;
+	let client;
+	try {
+		client = await connectDatabase();
+	} catch (error) {
+		res.status(500).json({ message: "Connecting to the database failed!" });
+		return;
+	}
 
-    const eventId = req.query.eventId;
+	if (req.method === "POST") {
+		const email = req.body.email;
+		const name = req.body.name;
+		const text = req.body.text;
 
-    if(req.method === "POST"){
+		if (
+			!email.includes("@") ||
+			!name ||
+			name.trim() === "" ||
+			!text ||
+			text.trim() === ""
+		) {
+			res.status(422).json({ message: "Invalid inputs." });
+			return;
+		}
 
-        const email = req.body.email;
-        const name = req.body.name;
-        const text = req.body.text;
+		const newComment = {
+			email,
+			name,
+			text,
+			eventId,
+		};
 
-        if(!email.includes('@') || !name || name.trim() === "" || !text || text.trim() === ""){
-            res.status(422).json({message: "Invalid inputs."});
-            return;
-        }
+		let result;
 
-        const newComment = {
-            email,
-            name,
-            text,
-            eventId
-        }
-        const filePath = buildfilePath('data','comments.json');
-        const data = extractFileData(filePath);
-        
-        data.push(newComment);
-        fs.writeFileSync(filePath,JSON.stringify(data));
-        res.status(201).json({message:"Success", comments:newComment});
-    }else{
-        const filePath = buildfilePath('data','comments.json');
-        const data = extractFileData(filePath);
-        res.status(200).json({message:"Success", comments:data});
-    }
+		try {
+			result = await insertDocument(client, "comments", newComment);
+			console.log("result", result);
+			newComment._id = result.insertedId;
+			res.status(201).json({ message: "Added comment.", comment: newComment });
+		} catch (error) {
+			res.status(500).json({ message: "Inserting comment failed!" });
+		}
+
+		const filePath = buildfilePath("data", "comments.json");
+		const data = extractFileData(filePath);
+
+		data.push(newComment);
+		fs.writeFileSync(filePath, JSON.stringify(data));
+	} else {
+		try {
+			const documents = await getAllDocuments(
+				client,
+				"comments",
+				{ _id: -1 },
+				{ eventId: eventId }
+			);
+			res.status(200).json({ message: "Success", comments: documents });
+		} catch (error) {
+			res.status(500).json({ message: "Getting comments failed!" });
+			return;
+		}
+
+		//fetching data from data folder start
+		const filePath = buildfilePath("data", "comments.json");
+		const data = extractFileData(filePath);
+		//end
+	}
+
+	client.close();
 }
--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,26 +1,66 @@
 import {
+	connectDatabase,
+	getAllDocuments,
+	insertDocument,
+} from "@/helpers/db-utils";
+import {
 	buildfilePath,
 	extractFileData,
 } from "@/helpers/file-path-extractData";
 import fs from "fs";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+	let client;
+
+	try {
+		client = await connectDatabase();
+	} catch (error) {
+		res.status(500).json({ message: "Connecting to the database failed!" });
+		return;
+	}
+
 	if (req.method === "POST") {
 		const userEmail = req.body.email;
 
-		if(!userEmail || !userEmail.includes("@")) {
+		if (!userEmail || !userEmail.includes("@")) {
 			res.status(422).json({ message: "Invalid email address." });
+			return;
 		}
 
 		const newNewsletter = { email: userEmail };
+
+		try {
+			await insertDocument(client, "newsletter", newNewsletter);
+		} catch (error) {
+			res.status(500).json({ message: "Inserting data failed!" });
+		}
+
+		//storing data inside project folder start
 		const filePath = buildfilePath("data", "newsletter.json");
 		const data = extractFileData(filePath);
 		data.push(newNewsletter);
-        fs.writeFileSync(filePath, JSON.stringify(data))
-		res.status(201).json({ message: "Success", newsletter: data });
-	} else {
+		fs.writeFileSync(filePath, JSON.stringify(data));
+		//end
+
+		res.status(201).json({ message: 'Signed up!' });
+	}  
+	
+	if (req.method === 'GET') {
+
+		//fetching data from data folder start
 		const filePath = buildfilePath("data", "newsletter.json");
 		const data = extractFileData(filePath);
-        res.status(200).json({ message: "Success", newsletter:data });
+		//end
+		
+		let storedData;
+		try{
+			storedData = await getAllDocuments(client, "newsletter", { _id: -1 });
+			res.status(200).json({ message: "Success", newsletter: storedData });
+		}catch(error){
+			res.status(500).json({message: 'Getting data failed!'});
+		}
+
 	}
+
+	client.close();
 }
